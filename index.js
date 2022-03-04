51b0a981d2808aead169105e4a82d542079b8677
enti <enti@enti.enti>
2022-03-04 12:06:06 +0100

    update JSON

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 #!/usr/bin/node
 
-
-
 let http = require("http");
-http.createServer(function (request, response) { 
-response.writeHead(200);
-let saludo = "<h1>ola k ase</h1>";
-response.end(saludo);
+let fs = require("fs");
+let mongo_client = require("mongodb").MongoClient;
+let url = "mongodb://localhost/";
 
+let db;
+
+console.log("Iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, conn){
+	console.log("Dentro de MongoDB");
+	
+	if (error) {
+		console.log("ERROR!");
+		return;
+		}
+
+	db = conn.db("tffhd");
+
+});
+
+http.createServer(function(request, response){
+	response.writeHead(200);
+	let archivo = "index.html";
+	if (request.url == "/") {
+		fs.readFile(archivo, function (err, data){
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.end(data);
+				});
+		
+		return;
+		}
+
+	let col = "";
+	
+	if(request.url == "/characters"){
+	col = "characters";
+	}
+	else if(request.url == "/items") {
+	col = "items";
+	}
+	else if (request.url == "/weapons"){
+	col = "weapons";
+	}
+	else{
+		response.end();
+		return;
+	}
+
+	let col_data = db.collection(col).find();
+	
+	col_data.toArray(function(error, data){
+		let string = JSON.stringify(data);
+		response.end(string);
+		});
 
 }).listen(1095);
+
+
+
 
