0fb934e50be6d593f660605df5ef6192a9a4d02e
enti <enti@enti.enti>
2022-05-16 00:05:24 +0200

    Items + Weapons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let http = require("http");
 let fs = require("fs");
 let mongo_client = require("mongodb").MongoClient;
 let url = "mongodb://localhost/";
-
+let ObjectId = require("mongodb").ObjectID;
 let db;
 
 console.log("Iniciando script mongo-http");
@@ -21,40 +21,97 @@ mongo_client.connect(url, function(error, conn){
 
 });
 
-http.createServer(function(request, response){
-	response.writeHead(200);
-	let archivo = "index.html";
-	if (request.url == "/") {
-		fs.readFile(archivo, function (err, data){
+
+function send_data_list (db, req, res){
+	let col = "";
+	let col_data = "";
+	if (req.url == "/characters"){
+		col = "characters";
+		col_data = db.collection(col).find({}, { projection: { name:1 } });
+	}
+
+	
+	else if(req.url == "/Items"){
+		col = "Items";
+		 col_data = db.collection(col).find({}, { projection: { item:1 } });
+	}
+	else if (req.url == "/weapons"){
+		col = "weapons";
+		 col_data = db.collection(col).find({}, { projection: { weapon:1 } });
+	}
+	else{
+		res.end();
+		return;
+	}
+
+
+	
+	col_data.toArray(function(err, data){
+		let string = JSON.stringify(data);
+		res.end(string);
+	});
+}
+
+http.createServer(function(req, res){
+	res.writeHead(200);
+	
+	if (req.url == "/") {
+		fs.readFile("index.html", function (err, data){
 				res.writeHead(200, {"Content-Type": "text/html"});
 				res.end(data);
 				});
 		
 		return;
 		}
+	let url = req.url.split("/");
 
-	let col = "";
-	
-	if(request.url == "/characters"){
-	col = "characters";
-	}
-	else if(request.url == "/items") {
-	col = "items";
-	}
-	else if (request.url == "/weapons"){
-	col = "weapons";
-	}
-	else{
-		response.end();
+	if (url.length==2){
+		send_data_list(db, req, res);
+		return;
+	}	
+
+	if (url[2].length != 24){
+
+		res.end();
 		return;
 	}
+	if(url[1]=="characters"){
+		let obj_id = new ObjectId(url[2]);
+		let col_data = db.collection("characters").find({"_id":obj_id}, {projection:{"name":1 }});
+			col_data.toArray(function(err, data){
+				let string = JSON.stringify(data);
+				res.end(string);
+			});
+		}
+	else if (url[1] == "Items") {
+		   let obj_id = new ObjectId(url[2]);
+		  
+		   let col_data = db.collection("Items").find({"_id":obj_id}, {projection:{"item":1}});
+			col_data.toArray(function(err, data){
+			 let string = JSON.stringify(data);
+			  res.end(string);
+			   });
+	}
+	else if (url[1] == "weapons") {
+		 let obj_id = new ObjectId(url[2]);
+		  let col_data = db.collection("weapons").find({"_id":obj_id}, {projection:{"weapon":1}});
+		  col_data.toArray(function(err, data){
+		  let string = JSON.stringify(data);
+		  res.end(string);
+		  });
+	}
+	else if (url[1] == "remove") {
+	  let obj_id = new ObjectId(url[2]);
+	  collectionName = url[3];
+	  db.collection(collectionName).deleteOne({"_id":obj_id});
+	  res.end("Deleted");
+	   
+	} 
 
-	let col_data = db.collection(col).find();
-	
-	col_data.toArray(function(error, data){
-		let string = JSON.stringify(data);
-		response.end(string);
-		});
+
+
+
+
 
 }).listen(1095);
 
